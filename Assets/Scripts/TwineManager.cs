ef684a273ce39a67eb142edbb28d60a08c7927b1
thk123 <thekileyenator@gmail.com>
2017-01-21 13:45:24 +0000

    Really basic Twine loading

---
+++ Assets/Scripts/TwineManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+public class TwineManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		XmlDocument xXml = new XmlDocument();
+		xXml.Load("Assets/Twine/RadioSpy.html");
+		XmlNodeList xNodeList = xXml.GetElementsByTagName("tw-passagedata");
+		print(xNodeList.Count);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
