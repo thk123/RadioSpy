8a7e36dfdea8dc4f61cc721d246b9c70df2f4cc5
thk123 <thekileyenator@gmail.com>
2017-01-21 16:39:21 +0000

    Scriptable object conversation

--- Assets/Scripts/ConversationManager.cs
+++ Assets/Scripts/ConversationManager.cs
@@ -4,13 +4,30 @@ using UnityEngine;
 
 public class ConversationManager : MonoBehaviour {
 
+	Conversation[] aConversations;
 	// Use this for initialization
 	void Start () {
-		
+		aConversations = (Conversation[]) Resources.FindObjectsOfTypeAll(typeof(Conversation));
+		LoadConversation(1, 1);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void LoadConversation(int iHouseNumber, int iDay)
+	{
+		Conversation xConversation = aConversations[iHouseNumber - 1];
+		StartCoroutine(PlayConversation(xConversation));
+	}
+
+	IEnumerator PlayConversation(Conversation xConversation) 
+	{
+	 	foreach(AudioClip statement in xConversation.aConversation)
+	 	{
+	 		AudioSource.PlayClipAtPoint(statement, Vector3.zero);
+	 		yield return new WaitForSeconds(statement.length);
+	 	}
 	}
 }
