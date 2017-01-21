2016bad44db805bf1c4f3ee8aa1ca2c3c4553535
Iulian Arcus <arcusiulian@gmail.com>
2017-01-21 20:53:46 +0000

    Added animations to knob and radio indicator.

--- Assets/RadioControls.cs
+++ Assets/RadioControls.cs
@@ -5,39 +5,63 @@ using UnityEngine;
 public class RadioControls : MonoBehaviour
 {
 
-    const float sfKnobStepRotationAngle = 10.0f;
-    const float sfRadioIndicatorStepDistance = 0.5f;
+    const float sfKnobStepRotationAngle = 5.0f;
+    const float sfRadioIndicatorStepDistance = 0.125f;
+    const float sfAnimationSpeed = 5.0f;
 
     uint muChannel = 0;
-    const uint suMaxChannels = 6; 
+    const uint suMaxChannels = 24;
+    GameObject mxKnob;
+    GameObject mxRadioIndicator;
 
-	// Use this for initialization
-	void Start ()
-    {
-		
-	}
-	
-	// Update is called once per frame
-	void Update ()
-    {
+    float mfChannelTransition = 0;
 
-        GameObject xKnob = GameObject.Find("Knob");
-        GameObject xRadioIndicator = GameObject.Find("RadioIndicator");
-        if (xKnob)
+
+    // Use this for initialization
+    void Start ()
+    {
+        mxKnob = GameObject.Find("Knob");
+        mxRadioIndicator = GameObject.Find("RadioIndicator");
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mxKnob && mxRadioIndicator)
         {
             if (Input.GetButtonDown("Channel Down") && muChannel > 0)
             {
-                xKnob.transform.Rotate(new Vector3(0, -sfKnobStepRotationAngle, 0));
-                xRadioIndicator.transform.Translate(new Vector3(-sfRadioIndicatorStepDistance, 0, 0));
                 muChannel--;
             }
 
-            if (Input.GetButtonDown("Channel Up") && muChannel < suMaxChannels - 1 )
+            if (Input.GetButtonDown("Channel Up") && muChannel < suMaxChannels - 1)
             {
-                xKnob.transform.Rotate(new Vector3(0, sfKnobStepRotationAngle, 0));
-                xRadioIndicator.transform.Translate(new Vector3(sfRadioIndicatorStepDistance, 0, 0));
                 muChannel++;
             }
         }
-	}
+
+        float fTimeDelta = Time.deltaTime * sfAnimationSpeed;
+
+        if (mfChannelTransition < muChannel - 0.05f)
+        {
+
+            mxKnob.transform.Rotate(new Vector3(0, sfKnobStepRotationAngle * fTimeDelta, 0));
+            mxRadioIndicator.transform.Translate(new Vector3(sfRadioIndicatorStepDistance * fTimeDelta, 0, 0));
+            mfChannelTransition += fTimeDelta;
+        }        
+        else if ( mfChannelTransition > muChannel + 0.05f)
+        {
+            mxKnob.transform.Rotate(new Vector3(0, -sfKnobStepRotationAngle * fTimeDelta, 0));
+            mxRadioIndicator.transform.Translate(new Vector3(-sfRadioIndicatorStepDistance * fTimeDelta, 0, 0));
+            mfChannelTransition -= fTimeDelta;
+
+        }
+        else if ( mfChannelTransition != muChannel)
+        {
+            mxKnob.transform.Rotate(new Vector3(0, sfKnobStepRotationAngle * (mfChannelTransition - muChannel), 0));
+            mxRadioIndicator.transform.Translate(new Vector3(sfRadioIndicatorStepDistance * (mfChannelTransition - muChannel), 0, 0));
+            mfChannelTransition = muChannel;
+        }
+    }
 }
