59e5c029f29cf19aa9a787c9e8828416dec4352f
thk123 <thekileyenator@gmail.com>
2017-01-21 23:57:27 +0000

    Generation of conversations

---
+++ Assets/Editor/GenerateConversations.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEditor;
+
+public class GenerateConversations
+{
+	[MenuItem("Assets/Generate/GenerateConversatiosn")]
+    public static void CreateConversation()
+    {
+    	LoadConversationsForFlat("Revolutionaries");
+        AssetDatabase.SaveAssets();
+    }
+
+    static void LoadConversationsForFlat(string sFlatName)
+    {
+    	XmlDocument xXml = new XmlDocument();
+		xXml.Load("Assets/Twine/" + sFlatName + ".html");
+		XmlNodeList xNodeList = xXml.GetElementsByTagName("tw-passagedata");
+		
+		Regex xNameMatcher = new Regex(@"^(?<day>\d+)\.(?<section>\d+):(?<room>\w+)\d*");
+
+		foreach(XmlNode xPassageNode in xNodeList)
+		{        	
+			string sPassageName = xPassageNode.Attributes["name"].Value;
+			Match xMatch = xNameMatcher.Match(sPassageName);
+
+			if(xMatch.Success)
+			{
+				Conversation asset = ScriptableObject.CreateInstance<Conversation>();
+
+				string sFileName = sPassageName.Replace('.', '_');
+
+        		AssetDatabase.CreateAsset(asset, 
+        			"Assets/Conversations/Generated/" + sFlatName + "/" + sFileName + ".asset");
+
+			}
+			else
+			{
+				Debug.LogWarning("Could not parse tag name" + sPassageName + " in flat " + sFlatName);
+			}
+		}
+    }
+}
--- Assets/Scripts/TwineManager.cs
+++ Assets/Scripts/TwineManager.cs
@@ -2,14 +2,47 @@
 using System.Collections.Generic;
 using System.Xml;
 using UnityEngine;
+using System.Text.RegularExpressions;
 
 public class TwineManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
+		LoadFlat("Revolutionaries", 2, 1, Action.NoAction());
+	}
+
+	Dictionary<string, Conversation> LoadFlat(string sFlatName, int iFlatNumber, int iDay, Action xActionTaken)
+	{
 		XmlDocument xXml = new XmlDocument();
-		xXml.Load("Assets/Twine/RadioSpy.html");
+		xXml.Load("Assets/Twine/" + sFlatName + ".html");
 		XmlNodeList xNodeList = xXml.GetElementsByTagName("tw-passagedata");
-		print(xNodeList.Count);
+		
+		Regex xNameMatcher = new Regex(@"^(?<day>\d+)\.(?<section>\d+):(?<room>\w+)\d*");
+
+		foreach(XmlNode xPassageNode in xNodeList)
+		{
+			string sPassageName = xPassageNode.Attributes["name"].Value;
+			Match xMatch = xNameMatcher.Match(sPassageName);
+
+			if(xMatch.Success)
+			{
+				int iDayOfPassage = System.Int32.Parse(xMatch.Groups["day"].Value);
+				if(iDay == iDayOfPassage)
+				{
+					sActionString = xActionTaken.ToString();
+					IEnumable<string> tags = xPassageNode.Attributes["tags"].Value.Split(' ');
+					if(tags == sActionString)
+					{
+
+					}
+				}
+				int iSection = System.Int32.Parse(xMatch.Groups["section"].Value);
+				string sRoom = xMatch.Groups["room"].Value;
+			}
+			else
+			{
+				Debug.LogWarning("Could not parse tag name" + sPassageName + " in flat " + sFlatName);
+			}
+		}
 	}
 	
 	// Update is called once per frame
