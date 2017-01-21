7cc9b7853739e2380a089ba7bd50961e64f62e0b
Iulian Arcus <arcusiulian@gmail.com>
2017-01-21 13:30:27 +0000

    Added keyboard setup for moving the knob.

---
+++ Assets/RadioControls.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RadioControls : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Channel Down"))
+        {
+            GameObject xKnob = GameObject.Find("Knob");
+            if (xKnob)
+            {
+                xKnob.transform.Rotate(new Vector3(0, -10, 0));
+            }
+        }
+	}
+}
