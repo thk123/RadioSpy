d0453a2b583a9f00c0f32c4ec6ef04f59c4f0bb2
thk123 <thekileyenator@gmail.com>
2017-01-22 15:52:51 +0000

    Basic game flow

---
+++ Assets/Scripts/GameFlow.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameFlow : MonoBehaviour {
+
+	// Use this for initialization
+	public RadioTextDisplayer mxRadioTextDisplayer;
+	public TextAsset mxBreifing;
+	void Start () {
+		StartCoroutine(RunGame());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	IEnumerator RunGame()
+	{
+		mxRadioTextDisplayer.DisplayText("RadioTap - Press SPACE to start", 
+			mxBreifing.text.Replace(Environment.NewLine, " "));
+		while (!Input.GetKeyDown(KeyCode.Space))
+		{
+         	yield return null;
+        }
+
+        var xCurrentRoomRadioController = 
+        	mxRadioTextDisplayer.gameObject.AddComponent<CurrentRoomRadioController>();
+
+        gameObject.GetComponent<RadioControls>().xChannelDisplayer = xCurrentRoomRadioController;
+
+        TwineManager xTwineManager = gameObject.GetComponent<TwineManager>();
+        xTwineManager.Begin(xCurrentRoomRadioController);
+
+
+	}
+}
