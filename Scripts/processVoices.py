91cb8464022a70f7093854de01b488348928f948
Iulian Arcus <arcusiulian@gmail.com>
2017-01-22 16:35:28 +0000

    Added all the voices

--- Scripts/processVoices.py
+++ Scripts/processVoices.py
@@ -5,13 +5,26 @@ import sys
 voiceSettings = [" -vfemale1 -p 80 -s 200", #G(female)
 		 " -vfemale3 -p 60 -s 200", #C(female)
 		 " -vmale2 -p 120 -s 230", #V(male)
-		 " -p 40 -s 200"] #F(male)
+		 " -ven-uk-wi -p 40 -s 200", #F(male)
+		 " -vfemale1 -p 100 -s 200", #R(female)
+		 " -vfemale3 -p 40 -s 250", #A(female)
+		 " -vfemale4 -p 60 -s 200", #L(female)
+		 " -ven-uk-wmids -p 70 -s 230", #D(male)
+		 " -vfemale5 -p 30 -s 230", #M(male)
+		 " -ven-uk-sc -p 60 -s 230", #K(male)
+		 ]
 speakerMapping = dict(
 					{
        				'G': 0,
         			'C': 1,
 					'V': 2,
-					'F': 3
+					'F': 3,
+					'R': 4,
+					'A': 5,
+					'L': 6,
+					'D': 7,
+					'M': 8,
+					'K': 9
     				})
 espeakPath = r'"C:\Program Files (x86)\eSpeak\command_line\espeak.exe" --path="C:\Program Files (x86)\eSpeak" '
 
